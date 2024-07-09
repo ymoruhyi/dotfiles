5e19afad06ea1f7c46bc64da1ffb4c428f80a0a2
Yevhenii Moruhyi <ymoruhyi@cisco.com>
2024-07-09 18:17:45 -0400

    fix: treesitter update for searching vim docs with Telescope

--- nvim/lua/ymoruhyi/plugins/treesitter.lua
+++ nvim/lua/ymoruhyi/plugins/treesitter.lua
@@ -1,4 +1,85 @@
-return {}
+return {
+  "nvim-treesitter/nvim-treesitter",
+  version = false,
+  build = ":TSUpdate",
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  event = { "VeryLazy" },
+  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+  keys = {
+    { "<c-space>", desc = "Increment Selection" },
+    { "<bs>", desc = "Decrement Selection", mode = "x" },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      config = true,
+      keys = {
+        { "<leader>uc", ":TSContextToggle<CR>", desc = "Toggle TSContext" },
+        {
+          "[c",
+          ":lua require('treesitter-context').go_to_context()<cr>",
+          silent = true,
+          desc = "Go to context",
+        },
+      },
+    },
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    "windwp/nvim-ts-autotag",
+  },
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
 -- return {
 --     -- Highlight, edit, and navigate code
 --     'nvim-treesitter/nvim-treesitter',
