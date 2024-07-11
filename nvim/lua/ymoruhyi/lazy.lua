3b6fc3f5b36fb2ff7bc0ff754916662097704de1
Yevhenii Moruhyi <ymoruhyi@cisco.com>
2024-07-11 17:12:23 -0400

    chore: remove commented plugins

--- nvim/lua/ymoruhyi/lazy.lua
+++ nvim/lua/ymoruhyi/lazy.lua
@@ -12,10 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- "tpope/vim-fugitive",
-  -- { "mbbill/undotree" },
-  -- { "tpope/vim-sleuth" },
-  -- "ThePrimeagen/git-worktree.nvim",
   { import = "ymoruhyi/plugins" },
 }, {
     defaults = { lazy = true },
